e3aba8ade16d5868e902421d11f5a388e4abc5c9
floatingnahid22 <nahid.uap.github@gmail.com>
2022-03-31 15:48:13 +0600

    update home.js and video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -8,7 +8,7 @@ export default function Video() {
         <img src={image} alt="Video Title" />
         <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
         <div className={classes.qmeta}>
-          <p>5 Questions</p>
+          <p>10 Questions</p>
           <p>Score : Not taken yet</p>
         </div>
       </div>
